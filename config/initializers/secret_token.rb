ef5c099a23e4e06b3879e65c72c582bfe67a2a64
Colin <colin.rymer@gmail.com>
2012-01-25 19:32:28 -0500

    Capstone on Rails: init commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Capstone::Application.config.secret_token = '78f462ce96e70a731787be5c0411124a535caa9dfe14bdf1f372d1a3aab8e186547ec472c3e16c4c853100ab86f95af68ca63a3382207cbaa8953c257144f03c'
