65d4d9b735919ae66c9ea8c27a04b5a56b8512b3
Colin <colin.rymer@gmail.com>
2012-01-28 01:26:12 -0500

    added twitter bootstrap 2.0. You're welcome, Reinsmidt.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,13 +6,13 @@ class SessionsController < ApplicationController
     user = User.find_by_email params[:email]
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      Rails.logger.info session[:user_id]
       redirect_to root_url, :notice => "Logged in!"
     else
       flash.now.alert = "Invalid email or password"
       render "new"
     end
   end
+
   def destroy
     session[:user_id] = nil
     redirect_to root_url, :notice => "Logged out!"
