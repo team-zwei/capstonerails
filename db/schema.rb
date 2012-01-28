78a7ec74374943099052b742bf9cc21262dd3889
Colin <colin.rymer@gmail.com>
2012-01-28 14:17:35 -0500

    enforce email uniqueness at db level to ensure uniqueness is true

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120127230306) do
+ActiveRecord::Schema.define(:version => 20120128191521) do
 
   create_table "auctions", :force => true do |t|
     t.string   "name"
@@ -25,5 +25,7 @@ ActiveRecord::Schema.define(:version => 20120127230306) do
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
   end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
 
 end
