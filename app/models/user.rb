138e1cd6c6f4d354697aaa5b7858540bafbc520f
Eric Haughee <ehaughee@gmail.com>
2012-01-30 10:44:26 -0500

    - Added username and username index to db migrations - Minor style tweaks

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,10 +10,11 @@
 #
 
 class User < ActiveRecord::Base
-  attr_accessible :email, :password, :password_confirmation
+  attr_accessible :email, :username, :password, :password_confirmation
   has_secure_password
 
   valid_email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, uniqueness: { case_sensitive: false }, presence: true, format: { with: valid_email_regex }
+  validates :username, presence: true, uniqueness: { case_sensitive: false }
   validates :password, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120129032601) do
+ActiveRecord::Schema.define(:version => 20120130153725) do
 
   create_table "auctions", :force => true do |t|
     t.string   "name"
@@ -24,8 +24,10 @@ ActiveRecord::Schema.define(:version => 20120129032601) do
     t.string   "password_digest"
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
+    t.string   "username"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["username"], :name => "index_users_on_username", :unique => true
 
 end
