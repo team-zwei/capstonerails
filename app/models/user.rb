30c4d1294c61ab05f5967752c610b1c8e75f4c47
Eric Haughee <ehaughee@gmail.com>
2012-02-06 10:56:52 -0500

    added password reset functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -32,4 +32,11 @@ class User < ActiveRecord::Base
   def make_admin
     self.update_attribute(:admin, true)
   end
+
+  def send_password_reset
+    generate_token :password_reset_token
+    self.password_reset_sent_at = Time.zone.now
+    save!
+    UserMailer.password_reset(self).deliver
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ Capstone::Application.routes.draw do
 
   resources :users
   resources :sessions
+  resources :password_resets
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
