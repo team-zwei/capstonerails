1b173f8c90ace31ee67316560cdbd651e0d6a5c0
ereinsmidt <eric@reinsmidt.com>
2012-02-15 10:52:12 -0500

    commiting some changes to _auction_list.html.erb

--- app/controllers/auctions_controller.rb
+++ app/controllers/auctions_controller.rb
@@ -3,4 +3,7 @@ class AuctionsController < ApplicationController
 		# TODO: Change to end_time when filled with data
 		@auctions = Auction.find(:all, :limit => 8, :order => 'created_at desc')
 	end
+	def show
+		@auction = Auction.find_by_id params[:id]
+	end
 end
