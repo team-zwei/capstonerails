a7e1fe56cfa67bd6ddb458cecaa4b684899dac69
Colin <colin.rymer@gmail.com>
2012-02-17 13:12:40 -0500

    added js login_status closure and moved background images to images folder

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
   helper_method :authenticate_admin_user!
   helper_method :current_admin_user
+  helper_method :logged_in?
 
   before_filter :require_login
 
