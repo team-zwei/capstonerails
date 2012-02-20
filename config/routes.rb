4ca814b15f684bbf24a7b12e437831b35afee49c
Eric Haughee <ehaughee@gmail.com>
2012-02-20 10:08:19 -0500

    added route for help page

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Capstone::Application.routes.draw do
   get "logout" => "sessions#destroy", as: "logout"
   get "login" => "sessions#new", as: "login"
   get "signup" => "users#new", as: "signup"
+  get "help" => "help#index", as: "help"
 
   root to: "home#index"
 
