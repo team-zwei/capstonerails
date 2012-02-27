fbbd1aad103c74024b3f9b22ada9c6747025d52b
Colin Rymer <crymer11@gmail.com>
2012-02-27 14:15:42 -0500

    Finished countdown timer and added it to auction list

--- app/assets/javascripts/countdown.js
+++ app/assets/javascripts/countdown.js
@@ -4,34 +4,51 @@ var countdownTimer = function(secondsRemaining, callback) {
 
   var cb = callback;
 
-  function init() {
-    tick();
-    timer = setInterval("tick()", 1000); // 1000 ms to tick every second.
-  }
+  tick();
+  timer = setInterval(tick, 1000); // 1000 ms to tick every second.
 
-  function tick(cb) {
+  function tick() {
     timeLeft -= 1;
     cb(renderTimeString());
   }
 
   function updateTime(seconds) {
     timeLeft = seconds;
+    console.log("time reset to " + seconds + " seconds");
   }
 
   function renderTimeString() {
     var time = timeLeft;
     var result = "";
-    result += parseInt(time / 86400).toString();
-    result += parseInt(time / 3600).toString();
-    result += parseInt((time / 60) % 60).toString();
-    result += parseInt(time % 60).toString();
+    result += formatNumber(time / 86400)     + ":";
+    result += formatNumber(time / 3600)      + ":";
+    result += formatNumber((time / 60) % 60) + ":";
+    result += formatNumber(time % 60);
+    return result;
+  }
+
+  function formatNumber(number) {
+    result = parseInt(number).toString();
+    if (result.length == 1) {
+      result = "0" + result;
+    }
     return result;
   }
 
   return {
     updateSecondsRemaining: function(newSecondsRemaining) {
-      updateTime(newSecondsRemaining)
+      updateTime(newSecondsRemaining);
     }
   }
 
 }
+
+var timers = {};
+
+$('.auction_thumbnail').each(function(){
+  var id = $(this).parent().attr('id')
+  var time_elem = $('#' + id + " .auction_thumbnail_time");
+  timers[id + "_timer"] = countdownTimer(time_elem.attr('data-time-remaining'), function(result){
+    time_elem.text(result);
+  });
+});
