afbb4b46439bc724a312318701ac02f9d2218c3e
Eric Haughee <ehaughee@gmail.com>
2012-03-07 12:09:26 -0500

    Home page no longer shows ended auctions

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,6 @@ class HomeController < ApplicationController
   skip_before_filter :require_login, only: [:index]
 
   def index
-  	@auctions = Auction.order("end_time desc").page(params[:page]).per(8)
+  	@auctions = Auction.where("end_time > now()").order("end_time desc").page(params[:page]).per(8)
   end
 end
