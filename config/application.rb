497c80dc56f02fcc5e69cde0c13aab47a6f9ef5d
Colin Rymer <crymer11@gmail.com>
2012-03-08 12:23:39 -0500

    fix typo for disable initialization on precompile

--- config/application.rb
+++ config/application.rb
@@ -56,6 +56,6 @@ module Capstone
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
-    config.assets.initialize_on_precompile to false
+    config.assets.initialize_on_precompile = false
   end
 end
