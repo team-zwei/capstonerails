8dfe8c72912abe8f861de4ac5a0c7baf26e84013
Colin Rymer <crymer11@gmail.com>
2012-03-08 21:19:56 -0500

    fixed nav link highlighting and updated production env config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ Capstone::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
@@ -31,7 +31,7 @@ Capstone::Application.configure do
   # config.force_ssl = true
 
   # See everything in the log (default is :info)
-  # config.log_level = :debug
+  config.log_level = :debug
 
   # Prepend all log lines with the following tags
   # config.log_tags = [ :subdomain, :uuid ]
