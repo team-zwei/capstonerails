d2230c5c12d060a9998498700b176dbc61eb1a17
Colin Rymer <crymer11@gmail.com>
2012-03-09 15:43:03 -0500

    update bid amount validation

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -15,17 +15,13 @@ class Bid < ActiveRecord::Base
   belongs_to :auction
   belongs_to :user
 
-  before_save :validate
+  before_save :validate_bid_amount
 
-  def validate
-  	current_bid_amount = Bid.find_by_id((self.auction.current_bid_id) ? self.auction.current_bid_id : self.auction.starting_bid_price).amount
-  	minimum_bid_increment = self.auction.minimum_bid_increment
-
-  	if self.amount < (current_bid_amount + minimum_bid_increment) || 
-      self.end_time < Time.now()
-  		false
-  	else
-  		true
-  	end
+  def validate_bid_amount
+    if self.auction.current_bid_id
+      return ((Bid.find_by_id(self.auction.current_bid_id).amount + self.auction.minimum_bid_increment) > self.amount) ? true : false
+    else
+      return (self.auction.starting_bid_price >= self.amount) ? true : false
+    end
   end
 end
