eae0f492fa7a87b425cac3d031c8b67c37ab137a
Colin Rymer <crymer11@gmail.com>
2012-04-12 17:08:31 -0400

    fleshing out image uploading

--- db/migrate/20120411143103_create_images.rb
+++ db/migrate/20120411143103_create_images.rb
@@ -2,6 +2,7 @@ class CreateImages < ActiveRecord::Migration
   def change
     create_table :images do |t|
     	t.string :auction_image
+      t.integer :auction_id
       t.timestamps
     end
   end
