217ed281536a0d11d7566184199cf5256643c6c6
Colin Rymer <crymer11@gmail.com>
2012-04-20 21:02:35 -0400

    making new auctions sexy

--- app/admin/administration.rb
+++ app/admin/administration.rb
@@ -5,6 +5,22 @@ end
 ActiveAdmin.register Auction do
   form :partial => "form"
   menu :parent => "Advanced"
+
+  controller do
+    layout 'active_admin'
+    def new
+      @page_title = "New Auction: Step 1 of 3"
+      @auction = Auction.new
+    end
+
+    def create
+      @auction = params[:auction]
+      render 'admin/auctions/image_form'
+    end
+
+
+
+  end
 end
 
 ActiveAdmin.register User do
