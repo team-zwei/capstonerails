6becbdbf34d13952a396e7da31ffdac5a5af9bae
Colin Rymer <crymer11@gmail.com>
2012-04-22 20:08:52 -0400

    at least mine works

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -6,12 +6,14 @@ class ImagesController < ApplicationController
 
   def new
     @image = Image.new
+    @auction = Auction.find_by_id params[:auction_id]
   end
 
   def create
     @image = Image.new params[:image]
-    @image.auction_id = Auction.find_by_id params[:auction_id]
+    @image.auction_id = Auction.find_by_id(params[:auction_id]).id
     if @image.save
+      Rails.logger.info(@image.auction_id)
       respond_to do |format|
         format.html {  
           render :json => [@image.to_jq_upload].to_json, 
