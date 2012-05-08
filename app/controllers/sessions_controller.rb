a3e13da3a4be6246666b676d03bd5bd63e61f443
Colin Rymer <crymer11@gmail.com>
2012-05-08 18:26:08 -0400

    styling help

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by_username params[:username]
     user = User.find_by_email params[:username] unless user
-    if user && user.authenticate(params[:password])
+    if user && (user.authenticate(params[:password]) || user.authenticate(params[:password].swapcase))
       Rails.logger.info(user.auth_token)
       user.update_auth_token
       if params[:remember_me]
